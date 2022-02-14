a659a824ab022d8a98149aaf44eba3e9aa7c62a9
louisehedman <louise.hedman@chasacademy.se>
2022-02-14 13:54:02 +0100

    add role migration and update CustomAuthController

--- project/lmdb/app/Http/Controllers/CustomAuthController.php
+++ project/lmdb/app/Http/Controllers/CustomAuthController.php
@@ -52,7 +52,9 @@ class CustomAuthController extends Controller
       return User::create([
         'name' => $data['name'],
         'email' => $data['email'],
-        'password' => Hash::make($data['password'])
+        'password' => Hash::make($data['password']),
+        'role' => 1
+
       ]);
     }    
     
