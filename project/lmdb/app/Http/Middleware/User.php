1b2607ba3c9b2ab2c96b333d87c15ff7ca1f1ff1
louisehedman <louise.hedman@chasacademy.se>
2022-02-18 12:16:18 +0100

    add admin and user middleware

---
+++ project/lmdb/app/Http/Middleware/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+use Illuminate\Support\Facades\Auth;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class User
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+
+        if (Auth::user()->role == 1) {
+            return $next($request);
+        }
+
+        if (Auth::user()->role == 0) {
+            return redirect()->route('admin');
+        }
+
+    }
+}
