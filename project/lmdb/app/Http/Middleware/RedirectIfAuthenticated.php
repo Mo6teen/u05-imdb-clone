69fbfe9601c0f9ead6409482f337a301c72b474d
louisehedman <louise.hedman@chasacademy.se>
2022-02-19 18:02:59 +0100

    update handle function to redirect to dashboard

--- project/lmdb/app/Http/Middleware/RedirectIfAuthenticated.php
+++ project/lmdb/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,7 +23,12 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                if (Auth::user()->role == 0){
+                    return redirect('admindashboard');
+                }
+                if (Auth::user()->role == 1){
+                    return redirect('userdashboard');
+                }
             }
         }
 
