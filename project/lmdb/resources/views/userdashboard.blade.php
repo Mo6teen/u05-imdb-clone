aafbfa4e5b3f799c2f92c2b37ce8a6980df28da7
louisehedman <louise.hedman@chasacademy.se>
2022-02-21 14:37:01 +0100

    update dashboards

--- project/lmdb/resources/views/userdashboard.blade.php
+++ project/lmdb/resources/views/userdashboard.blade.php
@@ -1,12 +1,23 @@
 @include('header')
-@extends('layouts.app')
+@extends('dashboard')
 
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="{{ url('css/app.css') }}">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+  <script src="{{ url('js/script.js') }}" defer></script>
+  <title>Dashboard</title>
+</head>
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Hi {{ Auth::user()->name }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
