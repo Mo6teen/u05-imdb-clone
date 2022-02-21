7ad0a3a2f46ebfbfb7f38345e9ca653dc4d2f685
louisehedman <louise.hedman@chasacademy.se>
2022-02-21 15:09:43 +0100

    update blade files

--- project/lmdb/resources/views/auth/login.blade.php
+++ project/lmdb/resources/views/auth/login.blade.php
@@ -1,4 +1,18 @@
+@include('header')
 @extends('dashboard')
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="{{ url('css/app.css') }}">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+  <script src="{{ url('js/script.js') }}" defer></script>
+  <title>Log in</title>
+</head>
+
 @section('content')
 <main class="login-form">
     <div class="cotainer">
@@ -32,7 +46,7 @@
                               <div class="col-md-6 offset-md-4">
                                   <div class="checkbox">
                                       <label>
-                                          <a href="{{ route('forget.password.get') }}">Reset Password</a>
+                                          <a href="{{ route('forget.password.get') }}">Forgot password? Click here</a>
                                       </label>
                                   </div>
                               </div>
