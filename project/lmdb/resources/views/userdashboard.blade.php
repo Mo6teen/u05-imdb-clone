56e191ff3a02d2bdec04ddfea73067fc29f88af9
louisehedman <louise.hedman@chasacademy.se>
2022-02-22 13:54:30 +0100

    create layout for userdashboard

--- project/lmdb/resources/views/userdashboard.blade.php
+++ project/lmdb/resources/views/userdashboard.blade.php
@@ -13,11 +13,14 @@
   <title>Dashboard</title>
 </head>
 @section('content')
+<main>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Hi {{ Auth::user()->name }}</div>
+                <div class="card-header">
+                    <h1 style="color: #fd7e14;">Hi {{ Auth::user()->name }}</h1>
+                    </div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -25,11 +28,49 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are in USER Dashboard!
-                </div>
+                    <h4 class="card-title dashboard-title">Welcome to your dashboard</h4>
+                    <div class="card-body">
+                        <h5 class="card-title">Here you can:</h5>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <a class="link-dark" href="#handlewatchlist">Handle your watchlist</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a class="link-dark" href="#handleotherlists">Handle other lists</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a class="link-dark" href="#manageusersettings">Manage your settings</a>
+                        </li>
+                    </ul>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                    <h2 id="handlewatchlist" style="color: #fd7e14;">Your watchlist</h2>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title"></h5>
+                    </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                    <h2 id="handleotherlists" style="color: #fd7e14;">Other lists</h2>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title"></h5>
+                    </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                    <h2 id="manageusersettings" style="color: #fd7e14;">Manage your settings</h2>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title"></h5>
+                    </div>
             </div>
         </div>
     </div>
 </div>
+</main>
+@include('footer')
 @endsection
