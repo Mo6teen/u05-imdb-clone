cf23adcca70d9f5eaf4a4f006e64232b46849234
louisehedman <louise.hedman@chasacademy.se>
2022-02-24 08:38:48 +0100

    add watchlist model and migration

---
+++ project/lmdb/app/Models/Watchlist.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Watchlist extends Model
+{
+    protected $table = 'watchlists';
+    protected $fillable = [
+        'user_id',
+        'movie_id',
+    ];
+
+}
