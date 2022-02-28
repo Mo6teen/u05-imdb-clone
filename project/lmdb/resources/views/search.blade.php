709035634b78c86daa0775a32c4a5cde3b85ba66
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-02-28 17:47:12 +0100

    searchBar is now done

--- project/lmdb/resources/views/search.blade.php
+++ project/lmdb/resources/views/search.blade.php
@@ -22,9 +22,13 @@
     <section class="py-5">
       <div class="container">
         <div class="row">
+          <div class="d-flex justify-content-between">
+            <h2>You searched for {{$_GET['title']}} </h2>
+          </div>
           @foreach ($movies as $movie)
           <div class="col-4 col-md-3 col-lg-3">
-            <img src="{{asset('images/LoTR 2.jpg')}}" alt="Random images" class="card-img-top">
+            <a href="/movie/{{ $movie->title }}">
+              <img src="{{url('/public/Image/' .$movie->image)}}" alt="Movie Thumbnail" class="card-img-top"></a>
             <div style="border: solid 0px transparent;  z-index: 0;">
               <p>{{$movie->title}}</p>
               <span style="display: inline;"><img src="{{asset('images/Star.png')}}" alt="Star">{{ $movie->rating }}</span>
