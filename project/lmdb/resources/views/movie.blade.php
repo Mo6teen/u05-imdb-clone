ace143d85a34bc6f329a7aef5acc4b1eae10a1cd
frinica <frida.nicander@chasacademy.se>
2022-03-03 12:41:50 +0100

    update forms in movie.blade and improve layout

--- project/lmdb/resources/views/movie.blade.php
+++ project/lmdb/resources/views/movie.blade.php
@@ -18,11 +18,13 @@
         </div>
 
         <!--Image and description section-->
-        <section>
+        <section class="container">
             <img src="{{asset('public/Image/'.$movie->image)}}" class="img-fluid mb-3" alt="Image">
-            <div class="d-flex justify-content-between mb-3">
-                <span style="display: inline;"><img src="{{asset('images/Star.png')}}" alt="Star">{{ $movie->rating }}</span>
-                <div class="card-body">
+            <div class="d-flex justify-content-around">
+                    <span style="display: inline;">
+                    <img src="{{asset('images/Star.png')}}" alt="Star">{{ $movie->rating }}</span>
+
+        <!-- Form to post to watchlist-->           
                     <form method="post" action="{{url('store-form')}}">
                         @csrf
                         <input hidden name="movie_id" value="{{ $movie->id }}">
@@ -31,11 +33,11 @@
                         @endauth
                         <div class="form-group">
                             <button type="submit" class="btn btn-primary">+ Watchlist</button>
-                        </form>
-                    </div>
+                        </div>
+                    </form>  
                 </div>
             </div>
-            <div>
+            <div class="row align-items-center">
                 <h2 class="display-6 mb-2">Description</h2>
                 <p class="f2-6">{{ $movie->description }}</p>
                 <p class="f2-6">{{ $movie->release_date }}</p>
@@ -44,13 +46,13 @@
         </section>
 
         <!-- Section to write and read reviews -->
-        <section>
+        <section class="container">
             <div class="mb-3">
                 <h2 class="card-title display-6">Reviews</h2>
-                <form class="card p-2" method="post" autocomplete="off" action="{{url('reviews-form')}}">
+                <form class="card p-2 align-items-center" method="post" autocomplete="off" action="{{url('reviews-form')}}">
                     @csrf
                     <input hidden name="movie_id" value="{{ $movie->id }}">
-                    <div class="form-group">
+                    <div class="form-group col-sm-7">
                         <label>Name</label>
                         @auth
                         <input type="text" id="title" name="name" class="form-control" readonly="readonly" value="{{ Auth::user()->name }}" required="">
@@ -58,14 +60,17 @@
                         <input type="text" id="title" name="name" class="form-control" readonly="readonly" value="You are not signed in" required="">
                         @endauth
                     </div>
-                    <div class="form-group">
+                    <div class="form-group col-sm-7">
                         <label>Review</label>
                         <textarea name="review" class="form-control" placeholder="Write your review" required=""></textarea>
                     </div>
-                    <button type="submit" class="btn btn-outline-dark m-3">Submit</button>
+                    <div class="form-group col-sm-7">
+                        <button type="submit" class="btn btn-outline-dark m-3">Submit</button>
+                    </div>
                 </form>
             </div>
 
+        <!-- Displaying reviews that has been approved by an admin -->
             @foreach($movie->reviews as $review)
             @if($review->approved == 1)
             <div class="card-header text-start">  
@@ -85,8 +90,8 @@
             @endif
             @endforeach
         </section>
+    
     </main>
-
     @include('footer')
     @endsection
 </body>
