99359618c786d642f26786d3c67b43b75d381976
frinica <frida.nicander@chasacademy.se>
2022-03-03 17:45:16 +0100

    update responsivity for genres.blade

--- project/lmdb/resources/views/genre.blade.php
+++ project/lmdb/resources/views/genre.blade.php
@@ -12,26 +12,30 @@
 @section('content')
   <main>
 
-    <section class="container m-3">
+    <section class="container">
       <div class="pb-3 text-center">
         <h2 class="display-1"> {{ $genre[0]->genre }}</h2>
       </div>
 
       <!-- Display movies in genre -->
+      <div class="d-flex justify-content-evenly">
         <div class="row">
           @foreach ($genre as $movie)
-          <div class="card p-1" style="width: 18rem;">
-            <a href="/movie/{{ $movie->title }}">
-              <img src="{{ url('/public/Image/' .$movie->image) }}" alt="{{ $movie->image }}" class="card-img-top pb-1">
-            </a>       
+          <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
+            <div class="card p-1 m-2" style="width: 18rem;">
+              <a href="/movie/{{ $movie->title }}">
+                <img src="{{ url('/public/Image/' .$movie->image) }}" alt="{{ $movie->image }}" class="card-img-top pb-1">
+              </a>       
               <h3 class="card-title">{{$movie->title}}</h3>
               <span style="display: inline;">
               <img src="{{asset('images/Star.png')}}" alt="Star">{{ $movie->rating }}</span>
+            </div>
           </div>
           @endforeach
-        </div>
+        </div> 
+      </div>  
     </section>
-    
+
   </main>
   @include('footer')
   @endsection
