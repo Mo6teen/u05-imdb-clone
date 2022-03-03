3760b25bd07e60fa274e2ace91151adefafa89ad
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-03-03 14:04:57 +0100

    fix bugg with genres page

--- project/lmdb/resources/views/genres.blade.php
+++ project/lmdb/resources/views/genres.blade.php
@@ -14,7 +14,7 @@
 
     <section class="py-5">
 
-      <h2>Genres</h2>
+      <h1>Genres</h1>
 
       <div class="container">
         <div class="d-flex justify-content-between">
@@ -30,22 +30,14 @@
             <a href="/movie/{{ $thriller->title }}">
               <img src="{{ url('/public/Image/' .$thriller->image) }}" alt="{{ $thriller->image }}" class="card-img-top">
             </a>
-          </div>
-          <div class="row">
-
-            @foreach ($genreThriller as $thriller)
-            <div class="card col-4">
-              <a href="/movie/{{ $thriller->title }}">
-                <img src="{{ url('/public/Image/' .$thriller->image) }}" alt="{{ $thriller->image }}" class="card-img-top">
-              </a>
-              <div class="card-body" style="border: solid 0px transparent;  z-index: 0;">
-                <h3 class="card-title">{{$thriller->title}}</h3>
-                <span class="card-text" style="display: inline;"><img src="{{asset('images/Star.png')}}" alt="Star">{{ $thriller->rating }}</span><br>
-              </div>
+            <div class="card-body" style="border: solid 0px transparent;  z-index: 0;">
+              <h3 class="card-title">{{$thriller->title}}</h3>
+              <span class="card-text" style="display: inline;"><img src="{{asset('images/Star.png')}}" alt="Star">{{ $thriller->rating }}</span><br>
             </div>
-            @endforeach
           </div>
+          @endforeach
         </div>
+      </div>
       </div>
     </section>
 
@@ -99,100 +91,89 @@
         </div>
       </div>
       </div>
-      <<<<<<< HEAD </section>
+    </section>
+
+    <section class="py-5">
+      <div class="container">
+        <div class="d-flex justify-content-between">
+
+          <a href="/genre/drama">
+            <h2>Comedy</h2>
+          </a>
+        </div>
+        <div class="row">
+
+          @foreach ($genreComedy as $comedy)
+          <div class="card col-4">
+            <a href="/movie/{{ $comedy->title }}">
+              <img src="{{ url('/public/Image/' .$comedy->image) }}" alt="{{ $comedy->image }}" class="card-img-top">
+            </a>
+            <div class="card-body" style="border: solid 0px transparent;  z-index: 0;">
+              <h3 class="card-title">{{$comedy->title}}</h3>
+              <span class="card-text" style="display: inline;"><img src="{{asset('images/Star.png')}}" alt="Star">{{ $comedy->rating }}</span><br>
+            </div>
+          </div>
+          @endforeach
+        </div>
+      </div>
+      </div>
+    </section>
+
+    <section class="py-5">
+      <div class="container">
+        <div class="d-flex justify-content-between">
+
+          <a href="/genre/drama">
+            <h2>Action</h2>
+          </a>
+        </div>
+        <div class="row">
+
+          @foreach ($genreAction as $action)
+          <div class="card col-4">
+            <a href="/movie/{{ $action->title }}">
+              <img src="{{ url('/public/Image/' .$action->image) }}" alt="{{ $action->image }}" class="card-img-top">
+            </a>
+            <div class="card-body" style="border: solid 0px transparent;  z-index: 0;">
+              <h3 class="card-title">{{$action->title}}</h3>
+              <span class="card-text" style="display: inline;"><img src="{{asset('images/Star.png')}}" alt="Star">{{ $action->rating }}</span><br>
+            </div>
+          </div>
+          @endforeach
+        </div>
+      </div>
+      </div>
+    </section>
+
+    <section class="py-5">
+      <div class="container">
+        <div class="d-flex justify-content-between">
+
+          <a href="/genre/drama">
+            <h2>Romance</h2>
+          </a>
+        </div>
+        <div class="row">
+
+          @foreach ($genreRomance as $romance)
+          <div class="card col-4">
+            <a href="/movie/{{ $romance->title }}">
+              <img src="{{ url('/public/Image/' .$romance->image) }}" alt="{{ $romance->image }}" class="card-img-top">
+            </a>
+            <div class="card-body" style="border: solid 0px transparent;  z-index: 0;">
+              <h3 class="card-title">{{$romance->title}}</h3>
+              <span class="card-text" style="display: inline;"><img src="{{asset('images/Star.png')}}" alt="Star">{{ $romance->rating }}</span><br>
+            </div>
+          </div>
+          @endforeach
+        </div>
+      </div>
+      </div>
+    </section>
 
   </main>
   @include('footer')
   @endsection
 </body>
-=======
-</div>
-</section>
-
-<section class="py-5">
-  <div class="container">
-    <div class="d-flex justify-content-between">
-
-      <a href="/genre/drama">
-        <h2>Comedy</h2>
-      </a>
-    </div>
-    <div class="row">
-
-      @foreach ($genreComedy as $comedy)
-      <div class="card col-4">
-        <a href="/movie/{{ $comedy->title }}">
-          <img src="{{ url('/public/Image/' .$comedy->image) }}" alt="{{ $comedy->image }}" class="card-img-top">
-        </a>
-        <div class="card-body" style="border: solid 0px transparent;  z-index: 0;">
-          <h3 class="card-title">{{$comedy->title}}</h3>
-          <span class="card-text" style="display: inline;"><img src="{{asset('images/Star.png')}}" alt="Star">{{ $comedy->rating }}</span><br>
-        </div>
-      </div>
-      @endforeach
-    </div>
-  </div>
-  </div>
-</section>
-
-<section class="py-5">
-  <div class="container">
-    <div class="d-flex justify-content-between">
-
-      <a href="/genre/drama">
-        <h2>Action</h2>
-      </a>
-    </div>
-    <div class="row">
-
-      @foreach ($genreAction as $action)
-      <div class="card col-4">
-        <a href="/movie/{{ $action->title }}">
-          <img src="{{ url('/public/Image/' .$action->image) }}" alt="{{ $action->image }}" class="card-img-top">
-        </a>
-        <div class="card-body" style="border: solid 0px transparent;  z-index: 0;">
-          <h3 class="card-title">{{$action->title}}</h3>
-          <span class="card-text" style="display: inline;"><img src="{{asset('images/Star.png')}}" alt="Star">{{ $action->rating }}</span><br>
-        </div>
-      </div>
-      @endforeach
-    </div>
-  </div>
-  </div>
-</section>
-
-<section class="py-5">
-  <div class="container">
-    <div class="d-flex justify-content-between">
-
-      <a href="/genre/drama">
-        <h2>Romance</h2>
-      </a>
-    </div>
-    <div class="row">
-
-      @foreach ($genreRomance as $romance)
-      <div class="card col-4">
-        <a href="/movie/{{ $romance->title }}">
-          <img src="{{ url('/public/Image/' .$romance->image) }}" alt="{{ $romance->image }}" class="card-img-top">
-        </a>
-        <div class="card-body" style="border: solid 0px transparent;  z-index: 0;">
-          <h3 class="card-title">{{$romance->title}}</h3>
-          <span class="card-text" style="display: inline;"><img src="{{asset('images/Star.png')}}" alt="Star">{{ $romance->rating }}</span><br>
-        </div>
-      </div>
-      @endforeach
-    </div>
-  </div>
-  </div>
-</section>
-
-</main>
-@include('footer')
-@endsection
-</body>
-
-</html>
->>>>>>> main
 
 </html>
