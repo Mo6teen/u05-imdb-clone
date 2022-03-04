0325153d343a80aa033af08e3907ec733faaa820
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-03-04 12:45:03 +0100

    fix footer on handleuser page

--- project/lmdb/resources/views/admindashboard.blade.php
+++ project/lmdb/resources/views/admindashboard.blade.php
@@ -10,7 +10,7 @@
 <body>
     @section('content')
     <main>
-        <div class="container py-5">
+        <div class="container py-5 my-5">
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     <div class="card">
