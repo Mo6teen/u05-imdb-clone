2ddf5dc66cbc8e0edcc2364a7a4d3c45ff4719e9
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-03-04 12:58:21 +0100

    fix margin and styling for usersettings page

--- project/lmdb/resources/views/usersettings.blade.php
+++ project/lmdb/resources/views/usersettings.blade.php
@@ -4,21 +4,22 @@
 @include('header')
 @extends('dashboard')
 @include('meta')
-    <title>LMDB - My settings</title>
+<title>LMDB - My settings</title>
 </head>
 @section('content')
 <main>
-    <div class="container"> 
+    <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">
-                          <h2 id="handleotherlists" style="color: #fd7e14;">My settings 
-                          @if(Auth::user()->role == 1)
-                          <a href="{{ url('userdashboard') }}" class="btn btn-dark float-end">BACK</a></h2>
-                          @elseif(Auth::user()->role == 0)
-                          <a href="{{ url('admindashboard') }}" class="btn btn-dark float-end">BACK</a></h2>
-                          @endif
+                        <h2 id="handleotherlists" style="color: #fd7e14;">My settings
+                            @if(Auth::user()->role == 1)
+                            <a href="{{ url('userdashboard') }}" class="btn btn-dark float-end">BACK</a>
+                        </h2>
+                        @elseif(Auth::user()->role == 0)
+                        <a href="{{ url('admindashboard') }}" class="btn btn-dark float-end">BACK</a></h2>
+                        @endif
                     </div>
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
@@ -26,23 +27,26 @@
                     </div>
                     @endif
                     <div class="card-body">
-                         <h5 class="card-title"></h5>
-                         <ul class="list-group list-group-flush">
-                             <li class="list-group-item h5">
-                                 <h5>Change your registered email</h5>
-                                 <a class="btn btn-dark btn-sm" href="{{ url('edit-email/') }}">Update email</a>
-                             </li>
-                             <li class="list-group-item h5">
-                                 <h5>Change your password</h5>
-                                 <a class="btn btn-dark btn-sm" href="{{ url('edit-password/') }}">Change password</a>
+                        <h5 class="card-title"></h5>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item h5">
+                                <h5>Change your registered email</h5>
+                                <a class="btn btn-dark btn-sm my-3" href="{{ url('edit-email/') }}">Update email</a>
                             </li>
-                         </ul>
+                            <li class="list-group-item h5 my-2">
+                                <h5>Change your password</h5>
+                                <a class="btn btn-dark btn-sm my-3" href="{{ url('edit-password/') }}">Change password</a>
+                            </li>
+                        </ul>
                     </div>
-                </div>     
+                </div>
             </div>
         </div>
+        <div class="py-5 my-5"></div>
+        <div class="py-5 my-5"></div>
 </main>
 @include('footer')
 @endsection
 </body>
+
 </html>
