501c683960a36e83f367b580827792cd0fd1a5f8
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-03-04 13:25:57 +0100

    fix styling for genre page

--- project/lmdb/resources/views/auth/registration.blade.php
+++ project/lmdb/resources/views/auth/registration.blade.php
@@ -4,7 +4,7 @@
 @include('header')
 @extends('dashboard')
 @include('meta')
-  <title>LMDB - Register</title>
+<title>LMDB - Register</title>
 </head>
 
 @section('content')
@@ -18,22 +18,19 @@
                         <form action="{{ route('register.custom') }}" method="POST">
                             @csrf
                             <div class="form-group mb-3">
-                                <input type="text" placeholder="Name" id="name" class="form-control" name="name"
-                                    required autofocus>
+                                <input type="text" placeholder="Name" id="name" class="form-control" name="name" required autofocus>
                                 @if ($errors->has('name'))
                                 <span class="text-danger">{{ $errors->first('name') }}</span>
                                 @endif
                             </div>
                             <div class="form-group mb-3">
-                                <input type="text" placeholder="Email" id="email_address" class="form-control"
-                                    name="email" required autofocus>
+                                <input type="text" placeholder="Email" id="email_address" class="form-control" name="email" required autofocus>
                                 @if ($errors->has('email'))
                                 <span class="text-danger">{{ $errors->first('email') }}</span>
                                 @endif
                             </div>
                             <div class="form-group mb-3">
-                                <input type="password" placeholder="Password" id="password" class="form-control"
-                                    name="password" required>
+                                <input type="password" placeholder="Password" id="password" class="form-control" name="password" required>
                                 @if ($errors->has('password'))
                                 <span class="text-danger">{{ $errors->first('password') }}</span>
                                 @endif
@@ -52,6 +49,8 @@
             </div>
         </div>
     </div>
+    <div class="py-5 my-5"></div>
+    <div class="py-5 my-5"></div>
 </main>
 @include('footer')
 @endsection
--- project/lmdb/resources/views/genre.blade.php
+++ project/lmdb/resources/views/genre.blade.php
@@ -21,18 +21,18 @@
         <div class="row">
           @foreach ($genre as $movie)
           <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
-            <div class="card p-1 m-2" style="width: 18rem;">
+            <div class="card p-1 m-2" style="width:350px">
               <a href="/movie/{{ $movie->title }}">
-                <img src="{{ url('/public/Image/' .$movie->image) }}" alt="{{ $movie->image }}" class="card-img-top pb-1">
-              </a>       
+                <img src="{{ url('/public/Image/' .$movie->image) }}" alt="{{ $movie->image }}" class="card-img-top pb-1" style="height: 450px;">
+              </a>
               <h3 class="card-title">{{$movie->title}}</h3>
               <span style="display: inline;">
-              <img src="{{asset('images/Star.png')}}" alt="Star">{{ $movie->rating }}</span>
+                <img src="{{asset('images/Star.png')}}" alt="Star">{{ $movie->rating }}</span>
             </div>
           </div>
           @endforeach
-        </div> 
-      </div>  
+        </div>
+      </div>
     </section>
 
   </main>
